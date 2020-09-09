2e18dccdca36cceb7cf4d3b524955792dced69f6
Zeb Rasco <31866081+zrasco@users.noreply.github.com>
2020-09-09 01:00:57 -0700

    Further work on batch builds; working towards making the files in a template format

--- HIDUPSResponder/test.bat
+++ HIDUPSResponder/test.bat
@@ -1,4 +1,5 @@
 @echo off
 echo The name of this file is: %~n0%~x0
 echo The name of this file is: %~nx0
+echo The directory this file is in: %~dp0
 echo The path of this file is: %~dp0HIDUPSResponder.exe
